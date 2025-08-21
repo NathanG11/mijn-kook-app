bebfbe356226cf2dd68b6104deacaed250cc4581
NathanG11 <nathan.goddefroy@gmail.com>
2025-08-21 14:54:28 +0200

    Versie 1.0 Finaliseren

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Onze Kook App',
+        short_name: 'Kook App',
+        description: 'Een slimme weekplanner voor ons gezin',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
